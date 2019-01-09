be1f139bc1df59de5065b6598731ea1366435bc3
Sotnikov, Dmitri <dmitri.sotnikov@gmail.com>
2019-01-09 12:09:40 -0500

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-library/lein-template "0.1.1"
+(defproject cljs-library/lein-template "0.1.2"
   :description "a template for ClojureScript libraries"
   :url "https://github.com/yogthos/figwheel-library-template"
   :license {:name "Eclipse Public License"
